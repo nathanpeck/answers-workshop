d956ac3265b319b1120d0faf099bcdea6c1fd79a
Nathan Peck <peckn@amazon.com>
2021-11-25 18:33:31 +0000

    Adding the API service

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -2,6 +2,12 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { VotingEnvironment } from '../lib/environment';
+import { APIService } from '../lib/api';
 
 const app = new cdk.App();
 const votingEnvironment = new VotingEnvironment(app, 'VotingEnvironmentWorkshop', {});
+
+const apiServiceStack = new APIService(app, "APIServiceWorkshop", {
+  ecsEnvironment: votingEnvironment.ecsEnvironment,
+  serviceDiscoveryName: votingEnvironment.serviceDiscoveryName
+});
